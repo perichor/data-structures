814fc4917e16160c20c1ac4f6a2406412a815b83
perichor <sandejeremy@gmail.com>
2016-12-14 16:09:47 -0800

    implement linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,43 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+
+    if (list.tail !== null) {
+      list.tail.next = newNode;
+    } else {
+      list.head = newNode;
+    }
+    list.tail = newNode;
   };
 
   list.removeHead = function() {
+    let currentHead = list.head;
+
+    // Test if there is only one node in the linked list
+    if (list.head === list.tail) {
+      list.tail = null;
+    }
+
+    list.head = list.head.next;
+
+    return currentHead.value;
   };
 
   list.contains = function(target) {
+
+    let currentNode = list.head;
+
+    let hasFoundMatchtoTarget = false;
+
+    while (currentNode !== null) {
+      if (currentNode.value === target) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+
+    return hasFoundMatchtoTarget; 
   };
 
   return list;
