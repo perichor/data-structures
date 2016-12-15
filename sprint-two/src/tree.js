9456f235b0698e5bcc23e54724d5d49bcff0f1da
perichor <sandejeremy@gmail.com>
2016-12-14 16:34:02 -0800

    implement tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,8 +2,9 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
+  newTree.children = [];
 
   return newTree;
 };
@@ -11,9 +12,22 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  let newChild = Tree(value);
+  this.children.push(newChild);
 };
 
 treeMethods.contains = function(target) {
+  var traverser = function(target, hasFound) {
+    if (this.value === target) {
+      hasFound = true;
+    } else {
+      this.children.forEach( function(child) {
+        hasFound = traverser.call(child, target, hasFound);
+      });
+    }
+    return hasFound;
+  };
+  return traverser.call(this, target, false);
 };
 
 
