60ae86ecc753caaebbef00ab1c96e4be7b706897
perichor <sandejeremy@gmail.com>
2016-12-16 14:51:21 -0800

    implement binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,65 @@
 var BinarySearchTree = function(value) {
+  let BSTInstance = {};
+  BSTInstance.left = undefined;
+  BSTInstance.right = undefined;
+  BSTInstance.value = value;
+
+  BSTInstance.insert = function(targetValue) {
+    if (this.value === targetValue) {
+      return undefined;
+    }
+
+    if (this.value > targetValue) {
+      if (this.left === undefined) {
+        this.left = BinarySearchTree(targetValue);
+      } else {
+        this.left.insert(targetValue);
+      }
+    }
+
+    if (this.value < targetValue) {
+      if (this.right === undefined) {
+        this.right = BinarySearchTree(targetValue);
+      } else {
+        this.right.insert(targetValue);
+      }
+    }
+
+  };
+
+  BSTInstance.contains = function(targetValue) {
+    if (this.value === targetValue) {
+      return true;
+    }
+
+    if (this.value > targetValue) {
+      if (this.left === undefined) {
+        return false;
+      } else {
+        return this.left.contains(targetValue);
+      }
+    }
+
+    if (this.value < targetValue) {
+      if (this.right === undefined) {
+        return false;
+      } else {
+        return this.right.contains(targetValue);
+      }      
+    }
+  };
+
+  BSTInstance.depthFirstLog = function(userFunction) {
+    userFunction(this.value);
+    if (this.left !== undefined) {
+      this.left.depthFirstLog(userFunction);
+    } 
+    if (this.right !== undefined) {
+      this.right.depthFirstLog(userFunction);
+    }
+  };
+
+  return BSTInstance;
 };
 
 
