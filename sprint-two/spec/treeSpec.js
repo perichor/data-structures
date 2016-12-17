5ad81b1694b709a8cc08c21900b82b796729d168
perichor <sandejeremy@gmail.com>
2016-12-17 10:31:23 -0800

    implement parent property and removeFrom parent method to tree. Added associated tests.

--- sprint-two/spec/treeSpec.js
+++ sprint-two/spec/treeSpec.js
@@ -5,10 +5,12 @@ describe('tree', function() {
     tree = Tree();
   });
 
-  it('should have methods named "addChild" and "contains", and a property named "value"', function() {
+  it('should have methods named "addChild", "removeFromParent", and "contains" and properties named "value" and "parent"', function() {
     expect(tree.addChild).to.be.a('function');
+    expect(tree.removeFromParent).to.be.a('function');
     expect(tree.contains).to.be.a('function');
     expect(tree.hasOwnProperty('value')).to.equal(true);
+    expect(tree.hasOwnProperty('parent')).to.equal(true);
   });
 
   it('should add children to the tree', function() {
@@ -36,9 +38,46 @@ describe('tree', function() {
     tree.addChild(5);
     tree.addChild(6);
     tree.children[0].addChild(7);
-    tree.children[1].addChild(8);
+    tree.children[0].addChild(8);
     expect(tree.contains(7)).to.equal(true);
     expect(tree.contains(8)).to.equal(true);
   });
+
+  it('should remove branch starting at target node', function() {
+    tree.addChild(5);
+    tree.addChild(6);
+    tree.children[0].addChild(7);
+    tree.children[1].addChild(8);
+
+    tree.children[0].children[0].addChild(3);
+    tree.children[0].children[0].addChild(4);
+    tree.children[0].children[0].addChild(5);
+
+    let removedBranch = tree.removeFromParent(7);
+    expect(removedBranch.contains(3)).to.equal(true);
+    expect(removedBranch.contains(4)).to.equal(true);
+    expect(removedBranch.contains(5)).to.equal(true);
+    expect(removedBranch.contains(7)).to.equal(true);
+
+    expect(tree.contains(7)).to.equal(false);
+    expect(tree.contains(3)).to.equal(false);
+    expect(tree.contains(4)).to.equal(false);
+  });
+
+  it('should return undefined if value specified to removeFromParent does not exist', function() {
+    tree.addChild(5);
+    tree.addChild(6);
+    tree.children[0].addChild(7);
+    tree.children[1].addChild(8);
+
+    tree.children[0].children[0].addChild(3);
+    tree.children[0].children[0].addChild(4);
+    tree.children[0].children[0].addChild(5);
+
+    let removedBranch = tree.removeFromParent(10);
+    expect(removedBranch).to.equal(undefined);
+
+  });
+
 
 });
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,15 @@
-var Tree = function(value) {
+var Tree = function(value, parent) {
   var newTree = {};
   newTree.value = value;
 
   newTree.addChild = treeMethods.addChild;
   newTree.contains = treeMethods.contains;
+  newTree.removeFromParent = treeMethods.removeFromParent;
+  if (parent === undefined) { 
+    newTree.parent = null;
+  } else {
+    newTree.parent = parent;
+  }
   newTree.children = [];
 
   return newTree;
@@ -12,8 +18,28 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-  let newChild = Tree(value);
+  let newChild = Tree(value, this);
   this.children.push(newChild);
+};
+
+treeMethods.removeFromParent = function(target) {
+  var traverser = function(target, hasFound) {
+    if (this.value === target) {
+      let currentParent = this.parent;
+      currentParent.children.splice(currentParent.children.indexOf(this), 1);
+      this.parent = null;
+      return this;
+
+    } else {
+      this.children.forEach( function(child) {
+        if (hasFound === undefined) { 
+          hasFound = traverser.call(child, target, hasFound);
+        }
+      });
+      return hasFound;
+    }
+  };
+  return traverser.call(this, target, undefined);
 };
 
 treeMethods.contains = function(target) {
